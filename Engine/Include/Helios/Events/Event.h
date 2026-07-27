687323d5d4a755b7a592e95e723a0378501087e5
Gauravved <gaurav.rpandit1@gmail.com>
2026-07-27 22:18:37 +0530

    Implemented Event base class

---
+++ Engine/Include/Helios/Events/Event.h
@@ -0,0 +1,71 @@
+#pragma once
+
+// Macro to get EventCategory
+#define EVENT_CLASS_TYPE(type)								\
+		static EventType GetStaticType(){					\
+			return EventType::type							\
+		}													\
+		virtual EventType GetEventType() const override{	\
+			return GetStaticType();							\
+		}													\		
+		virtual const char* GetName() const override{		\
+			return #type;									\
+		}													\
+
+
+#define EVENT_CLASS_CATEGORY(category)						\
+		virtual int GetCategoryFlags() const override{		\
+			return category;								\
+		}													\
+
+namespace Helios {
+	enum class EventType {
+		None = 0,
+
+		// Application Events
+		WindowClose,
+		WindowResize,
+
+		// Keyboard Events
+		KeyPressed,
+		KeyReleased,
+
+		// Mouse Events
+		MouseButtonPressed,
+		MouseButtonReleased,
+		MouseMoved,
+		MouseScrolled
+	};
+
+	enum EventCategory {
+		None = 0,
+
+		EventCategoryApplication		= 1 << 0,
+		EventCategoryInput				= 1 << 1,
+		EventCVategoryKeyboard			= 1 << 2,
+		EventCategoryMouse				= 1 << 3,
+		EventCategoryMMouseButton		= 1 << 4
+	};
+
+	class Event {
+	public:
+
+		virtual ~Event() = default;
+
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags()const = 0;
+
+		bool IsInCategory(EventCategory category) {
+			return GetCategoryFlags() & category;
+		}
+
+	public:
+		bool Handled = false;
+		
+	protected:
+	
+	private:
+
+	};
+}
